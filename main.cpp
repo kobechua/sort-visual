176d75650c8cf96330abab9eb87cc5c5abc182a3
kobechua <kobechua14@gmail.com>
2024-06-12 09:50:07 -0400

    added new sorting algorithms

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <random>
-#include <thread>
+#include <time.h>
 #include <unistd.h>
 #include <SFML/Graphics.hpp>
 
@@ -10,17 +10,21 @@
 #define PADDING 20
 #define HERE "*"
 
-void visual(sf::RenderWindow& window, std::vector<int> el) {
+void visual(sf::RenderWindow& window, std::vector<int> el, int m) {
     int n = el.size();
     for (int i=0; i < n; i++){
         int h, w, x, y;
         h = el[i] * ((HEIGHT - 2 * PADDING) / (n-1));
         w = (WIDTH - 20 * PADDING) / (n-1);
         x = 200 + (i * 1.1 * w); 
-        y = HEIGHT;
+        y = HEIGHT - PADDING;
         
-        std::cout << el[i] << " " << h << " " << w << " " << x << " " << y << std::endl;
+        //std::cout << el[i] << " " << h << " " << w << " " << x << " " << y << std::endl;
         sf::RectangleShape rectangle(sf::Vector2f(w, h));
+        
+        if (m == el[i]) {
+            rectangle.setFillColor(sf::Color(255, 0, 0));
+        }
 
         rectangle.setPosition(x, y);
         rectangle.rotate(180);
@@ -28,13 +32,12 @@ void visual(sf::RenderWindow& window, std::vector<int> el) {
     }
     window.display();
 
-    std::cout << std::endl;
+    //std::cout << std::endl;
 }
 
 void bubbleSort(sf::RenderWindow& window, std::vector<int>& arr, int n) {
     int i, j;
     bool swapped;
-    sleep(2);
     for (i = 0; i < n - 1; i++) {
         swapped = false;
         for (j = 0; j < n - i - 1; j++) {
@@ -42,14 +45,52 @@ void bubbleSort(sf::RenderWindow& window, std::vector<int>& arr, int n) {
             if (arr[j] > arr[j + 1]) {
                 swapped = true;
                 std::swap(arr[j], arr[j+1]);
-                sleep(0.5);
                 window.clear();
-                visual(window, arr);
+                visual(window, arr, arr[j]);
             }
         }
 
         if (swapped == false)
             break;
+    }
+}
+
+void selectionSort(sf::RenderWindow& window, std::vector<int>& arr, int n)
+{
+    int i, j, min_idx;
+
+    for (i = 0; i < n - 1; i++) {
+
+       min_idx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+
+       if (min_idx != i) {
+            std::swap(arr[min_idx], arr[i]);
+            window.clear();
+            usleep(100000);
+            visual(window, arr, arr[min_idx]);
+        }
+    }
+}
+
+void insertionSort(sf::RenderWindow& window, std::vector<int>& arr, int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+        usleep(100000);
+        window.clear();
+        visual(window, arr, key);
     }
 }
 
@@ -61,22 +102,21 @@ void randomize(std::vector<int>& el) {
     std::mt19937 gen(rd());
     std::shuffle(el.begin(), el.end(), gen);
 
-    for (int i : el) {
-        std::cout << i << std::endl;
-    }
+    /*for (int i : el) {
+        nstd::cout << i << std::endl;
+    }*/
 
-    std::cout << std::endl;
+    //std::cout << std::endl;
 
 }
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Sorting Visualization");
-    //sf::RectangleShape rectangle(sf::Vector2f(20.f, 20.f));
-    //rectangle.setPosition(300, 200);
+ 
     window.setFramerateLimit(60);
     
     //ask for user input
-    int n = 100;
+    int n = 37;
 
     std::vector<int> elements(n);
 
@@ -94,8 +134,8 @@ int main() {
 
         window.clear();
         
-        bubbleSort(window, elements, n);
-        visual(window, elements);
+        insertionSort(window, elements, n);
+        visual(window, elements, -1);
         window.display();
 
     }
